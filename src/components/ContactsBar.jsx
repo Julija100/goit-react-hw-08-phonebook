52a157de287c3091199f58c9420b0ef9f63b1d79
Julija Faltina <dashinicha@gmail.com>
2021-10-11 13:11:31 +0300

    Realise Contacts Filter

---
+++ src/components/ContactsBar.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+import { styled } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+import ContactsFilter from './ContactsFilter';
+
+const AddContactBtn = styled(Fab)({
+  position: "absolute",
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: "0 auto",
+  opacity: "0.8",
+});
+
+export default function ContactsBar({ openAddContactModal }) {
+    const 
+}
