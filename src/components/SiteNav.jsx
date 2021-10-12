6a52ad3c9d0c57d6a66a40ac012c4eaab1681413
Julija Faltina <dashinicha@gmail.com>
2021-10-12 18:02:28 +0300

    Realise site navigation

--- src/components/SiteNav.jsx
+++ src/components/SiteNav.jsx
@@ -1,9 +1,37 @@
 import { useSelector } from "react-redux";
 import { useMediaQuery } from "@mui/material";
 
-import { getIsLoggedIn } from "redux/auth/authSelectors";
-import { theme } from "common/theme";
+import { getIsLoggedIn } from "../redux/authorisation/authSelectors";
+import { theme } from "../common/theme";
 import NavList from "./NavList";
 import NavItem from "./NavItem";
 import NavLinkRouter from "./NavLinkRouter";
-import Logo from "./Logo";
+// import Logo from "./Logo";
+
+
+export default function SiteNav() {
+    const isLoggedIn = useSelector(getIsLoggedIn);
+    const isAfterSmallScreen = useMediaQuery(theme.breakpoints.up('sm'));
+
+    return (
+      <NavList className="list">
+        <NavItem>
+          <NavLinkRouter
+            homeLink
+            to="/"
+            className="link"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            {isAfterSmallScreen && "Phonebook"}
+          </NavLinkRouter>
+        </NavItem>
+        {isLoggedIn && (
+          <NavItem>
+            <NavLinkRouter to="/contacts" className="link">
+              Contacts
+            </NavLinkRouter>
+          </NavItem>
+        )}
+      </NavList>
+    );
+}
