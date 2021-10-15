b207db2cff4be5f9320fe79afcc57af0f4b17734
Julija Faltina <dashinicha@gmail.com>
2021-10-15 17:10:21 +0300

    Fix router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import { ThemeProvider } from "@mui/system";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -7,13 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import { persistor, store } from "./redux/store";
-import { theme } from './common/theme'
+import { theme } from "./common/theme";
+import { homepage } from "../package.json";
+
+const { pathname: basename } = new URL(homepage);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename={basename}>
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
@@ -23,4 +25,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
